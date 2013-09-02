7398ff85a03bf938822cd655ef63a2a1557327d1
Brion Vibber <brion@pobox.com>
2013-09-01 18:20:47 -0700

    initial stub

--- src/com/brionv/HybridHost/HybridHostActivity.java
+++ src/com/brionv/HybridHost/HybridHostActivity.java
@@ -1,9 +1,14 @@
 package com.brionv.HybridHost;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.*;
 
 public class HybridHostActivity extends Activity {
+    private WebView webView;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +16,36 @@ public class HybridHostActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        webView = (WebView)findViewById(R.id.hybridHostWebView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setBlockNetworkLoads(false);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                return super.shouldInterceptRequest(view, url);    //To change body of overridden methods use File | Settings | File Templates.
+            }
+        });
+
+        loadFirstPage();
+    }
+
+    private void loadFirstPage() {
+        loadUrl("http://en.m.wikipedia.org/");
+    }
+
+    private void loadUrl(String url) {
+        webView.loadUrl(url);
+    }
+
+    private void openUrlInExternalBrowser(String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
