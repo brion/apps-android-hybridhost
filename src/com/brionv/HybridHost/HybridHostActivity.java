f1817a4a29ddbc4fee5e0a7cd513f259ccc8b64b
Brion Vibber <brion@pobox.com>
2013-09-01 17:25:01 -0700

    stub app

---
+++ src/com/brionv/HybridHost/HybridHostActivity.java
@@ -0,0 +1,15 @@
+package com.brionv.HybridHost;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HybridHostActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
